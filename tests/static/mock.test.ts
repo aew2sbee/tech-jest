bd589ab2e3cd0f5810228a507a2a3c8e624fece7
aew2sbee <aew2sbee@gmail.com>
2026-02-03 21:33:27 +0900

    test: isEven関数のモック実装を追加し、動作を確認するテストを実施

--- tests/static/mock.test.ts
+++ tests/static/mock.test.ts
@@ -1,3 +1,17 @@
+import * as calc from "../../src/server/lib/calc";
+
+// "../../src/server/lib/calc"モジュール全体をモックする
+// ポイント: モジュールのパスが必須
+jest.mock("../../src/server/lib/calc", () => ({
+  // jest.requireActual()を使って、モック対象のモジュールの実際の関数をすべて取得する
+  // これにより、isEven以外の関数はそのままの動作を維持する
+  ...jest.requireActual("../../src/server/lib/calc"),
+  // isEven関数をモックする
+  // jest.fn()でモック関数を作成し、モックの実装を定義する
+  // このモックされたisEvenは、引数が99の場合にのみtrueを返し、それ以外はfalseを返す
+  isEven: jest.fn((num: number) => num === 99),
+}));
+
 describe("モックオブジェクトの評価", () => {
   test("jest.fn().mockImplementationで掛け算が可能か確認する", () => {
     const mockFunction = jest.fn().mockImplementation((x: number) => x * 2);
@@ -11,5 +25,14 @@ describe("モックオブジェクトの評価", () => {
     expect(mockFunction()).toBe("1回目");
     expect(mockFunction()).toBe("2回目");
     expect(mockFunction()).toBe(undefined);
+  });
+});
+
+describe("isEven関数のモックテスト", () => {
+  test("isEvenがモックされていることを確認する", () => {
+    // モックされたisEvenは、入力が99の場合のみtrueを返す
+    expect(calc.isEven(99)).toBe(true);
+    expect(calc.isEven(2)).toBe(false);
+    expect(calc.isEven).toHaveBeenCalledTimes(2);
   });
 });
